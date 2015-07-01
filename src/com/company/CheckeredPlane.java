7e30b0f1b20d999a765451e32de1f28ade0f86a1
AlexanderLeon <lav09.01.95@gmail.com>
2015-07-01 20:40:41 +0300

    Правки

--- src/com/company/CheckeredPlane.java
+++ src/com/company/CheckeredPlane.java
@@ -24,33 +24,9 @@ public class CheckeredPlane
 		}
 	}
 
-	CheckeredPlane()
+	CheckeredPlane(char [][] inPlane)
 	{
-		lab[0][0] = '#';
-		lab[0][1] = '#';
-		lab[0][2] = '#';
-		lab[0][3] = '#';
-		lab[0][4] = '#';
-		lab[1][0] = '#';
-		lab[1][1] = ' ';
-		lab[1][2] = ' ';
-		lab[1][3] = ' ';
-		lab[1][4] = '#';
-		lab[2][0] = '#';
-		lab[2][1] = ' ';
-		lab[2][2] = '#';
-		lab[2][3] = ' ';
-		lab[2][4] = '#';
-		lab[3][0] = '#';
-		lab[3][1] = ' ';
-		lab[3][2] = ' ';
-		lab[3][3] = '#';
-		lab[3][4] = '#';
-		lab[4][0] = '#';
-		lab[4][1] = '#';
-		lab[4][2] = '#';
-		lab[4][3] = '#';
-		lab[4][4] = '#';
+
 
 		labHeight = 5;
 		labWidth = 5;
